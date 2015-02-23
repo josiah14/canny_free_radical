820deb76981957dd2d0c32648433392fee2dbafe
josiah14 <praenato14@gmail.com>
2015-02-22 23:42:07 -0600

    nix mangling and compressing of assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,12 +25,12 @@ Rails.application.configure do
   config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(mangle: false)
+  # config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
   # fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
