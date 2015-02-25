473145cf30b2e3df181d69d201ef373b611ca208
josiah14 <praenato14@gmail.com>
2015-02-24 22:53:40 -0600

    bower components seem to be functioning in dev

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,11 @@
-//= require bootstrap-sprockets
-//= require angular
-//= require angular-animate
-//= require angular-route
-//= require angular-resource
-//= require angular-ui-bootstrap
-//= require angular-ui-bootstrap-tpls
+//= require jquery/dist/jquery.min
+//= require bootstrap-sass-official/assets/javascripts/bootstrap-sprockets
+//= require angular/angular.min
+//= require angular-animate/angular-animate.min
+//= require angular-route/angular-route.min
+//= require angular-resource/angular-resource.min
+//= require angular-bootstrap/ui-bootstrap.min
+//= require angular-bootstrap/ui-bootstrap-tpls.min
 //= require turbolinks
 //= require_tree .
 
