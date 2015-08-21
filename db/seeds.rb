9bf12879771f406c4d1b6003c7af699eef03ded2
josiah14 <praenato14@gmail.com>
2015-08-21 08:19:56 -0500

    update test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,89 +8,89 @@
 
 posts_list = [
           [
-            "KISS and KIDS",
-            "kiss-and-kids.png",
-            "-KISS = Keep It Simple Stupid\n-KIDS == Keep It Dry Stupid\n\nThis is a rant on how to not write 'stupid' code into your programs.  Whether novice or elite, it is essential, if you want to become a respectable programmer, to learn the mantra, Don't Repeat Yourself and don't add complexity where it need not be.  blah blah so on and so forth until I'm done complaining about how few people tend to follow these basic, simple guidelines to produce maintainable code, even when moving swiftly."
+            'KISS and KIDS',
+            'kiss-and-kids.png',
+            "- KISS = Keep It Simple Stupid\n-KIDS == Keep It Dry Stupid\n\nThis is a rant on how to not write 'stupid' code into your programs.  Whether novice or elite, it is essential, if you want to become a respectable programmer, to learn the mantra, Don't Repeat Yourself and don't add complexity where it need not be.  blah blah so on and so forth until I'm done complaining about how few people tend to follow these basic, simple guidelines to produce maintainable code, even when moving swiftly."
           ],
           [
-            "Something Whimsical",
-            "something-whimsical.png",
-            "I'm writing something senseless and whimsical here. lah-dee-dah-doo-day.  This is a short post, but hey, at least it has a \n#Header"
+            'Something Whimsical',
+            'something-whimsical.png',
+            "I'm writing something senseless and whimsical here. lah-dee-dah-doo-day.  This is a short post, but hey, at least it has a \n# Header"
           ],
           [
-            "An Introduction to Prolog",
-            "spock-logic.jpg",
-            "Prolog post text goes here..."
+            'An Introduction to Prolog',
+            'spock-logic.jpg',
+            'Prolog post text goes here...'
           ],
           [
-            "An Overview of the Phoenix Framework",
-            "phoenixframework.png",
-            "fourth post text goes here..."
+            'An Overview of the Phoenix Framework',
+            'phoenixframework.png',
+            'fourth post text goes here...'
           ],
           [
-            "A First Glance at ChainFlow",
-            "chainflow-glance.png",
-            "fifth post text goes here..."
+            'A First Glance at ChainFlow',
+            'chainflow-glance.png',
+            'fifth post text goes here...'
           ],
           [
-            "Sixth Post",
-            "sixth-post.png",
-            "sixth post text goes here..."
+            'Sixth Post',
+            'sixth-post.png',
+            'sixth post text goes here...'
           ],
           [
-            "This is the Seventh Post, which is about Monads (again)!",
-            "seventh-post.png",
-            "seventh post text goes here..."
+            'This is the Seventh Post, which is about Monads (again)!',
+            'seventh-post.png',
+            'seventh post text goes here...'
           ],
           [
-            "Eighth Post",
-            "eighth-post.jpg",
-            "eighth post text goes here..."
+            'Eighth Post',
+            'eighth-post.jpg',
+            'eighth post text goes here...'
           ],
           [
-            "Nineth Post",
-            "nineth-post.jpg",
-            "nineth post text goes here..."
+            'Nineth Post',
+            'nineth-post.jpg',
+            'nineth post text goes here...'
           ],
           [
-            "About Atomicity",
-            "tenth-post.jpg",
-            "tenth post text goes here..."
+            'About Atomicity',
+            'tenth-post.jpg',
+            'tenth post text goes here...'
           ],
           [
             "Elm's Time Travelling Debugger Examined",
-            "tardis.jpg",
-'# Some header
+            'tardis.jpg',
+            '# Some header
 
 ```ruby
 # this is an innocuous method that does nothing important
 def method(arg)
-  puts arg.to_s if arg.respond_to(:to_s)
-  fail ArgumentError "The arg provided to #{__method__} must be stringifiable"
+puts arg.to_s if arg.respond_to(:to_s)
+fail ArgumentError "The arg provided to #{__method__} must be stringifiable"
 end
 ```
 
 ```javascript
 /*
- * Some comment
- */
+* Some comment
+*/
 globalVar = 5;
 var localFunc = function (arg) {
-      // log information to the console
-      console.log(arg);
-      return {
+    // log information to the console
+    console.log(arg);
+    return {
         publicFunction: function () { return "hello world!"; }
-      }
+    }
     },
     aHash = {
-      \'one\': 1,
-      "two": 2
+    \'one\': 1,
+    "two": 2
     };
 
 function anotherFunk(args) {
-  $(document).on(\'load\', function () {
+$(document).on(\'load\', function () {
     console.log(\'loaded\');
-  });
+});
 }
 
 ```
@@ -98,12 +98,12 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at purus eli
 
 ```haskell
 data Person = { firstName :: String
-               , lastName :: String
-               , age :: Int
-               , height :: Float
-               , phoneNumber :: String
-               , favor :: String
-               } deriving Show
+            , lastName :: String
+            , age :: Int
+            , height :: Float
+            , phoneNumber :: String
+            , favor :: String
+            } deriving Show
 
 data Maybe a = Nothing | Just a
 
